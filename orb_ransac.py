64925bbcc222604c354681b619a925566e997658
toda-hibiki <43288669+toda-hibiki@users.noreply.github.com>
2019-07-22 13:05:52 +0900

    Add files via upload

---
+++ orb_ransac.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 10
+good_match_rate = 0.15
+
+img1 = cv2.imread('./2019/0520/kekka-ab.png',0) # queryImage
+img2 = cv2.imread('./2019/0520/ab.png',0) # trainImage
+ 
+# Initiate ORB detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFmatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+#bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=False)
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+
+matches = sorted(matches, key = lambda x:x.distance)
+good = matches[:int(len(matches) * good_match_rate)]
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print ("Not enough matches are found - %d/%d") % (len(good),MIN_MATCH_COUNT)
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
